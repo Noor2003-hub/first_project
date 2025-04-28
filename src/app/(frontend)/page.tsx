8d16da719653f1139e4ae6bf311f505c0ca6cdb7
Noor2003-hub <sun.n00r.n@gmail.com>
2025-04-28 23:29:20 +0300

    update both 5

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,20 +1,55 @@
-import { notFound } from 'next/navigation'
-import { getPayload } from 'payload'
+
 import config from '@payload-config'
+import { getPayload } from 'payload'
+import React, { cache } from 'react'
+
+// import type { Page as PageType } from '../../../payload-types'
+
 import { RenderBlocks } from '@/utils/RenderBlocks'
+import { notFound } from 'next/navigation'
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const slug = decodeURIComponent(params.slug || 'index')
+const queryPageBySlug = cache(async ({ slug }: { slug: string }) => {
 
-  const payload = await getPayload({ config })
-
-  const { docs } = await payload.find({
-    collection: 'pages',
-    limit: 1,
-    where: { slug: { equals: slug } },
+    const parsedSlug = decodeURIComponent(slug)
+  
+    const payload = await getPayload({ config })
+  
+    const result = await payload.find({
+      collection: 'pages',
+      limit: 1,
+      where: {
+        slug: {
+          equals: parsedSlug,
+        },
+      },
+    })
+  
+    return result.docs?.[0] || null
   })
 
-  const page = docs[0]
+
+export async function generateStaticParams() {
+  const payload = await getPayload({ config })
+  const pages = await payload.find({
+    collection: 'pages',
+    draft: false,
+    limit: 1000,
+  })
+
+  return pages.docs
+    ?.filter((doc) => {
+      return doc.slug !== 'index'
+    })
+    .map(({ slug }) => slug)
+}
+
+
+export default async function Page({ params: { slug = 'index' } }) {
+  let page: any
+
+  page = await queryPageBySlug({
+    slug,
+  })
 
   if (!page) {
     return notFound()
@@ -22,7 +57,10 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
   return (
     <article className="pt-16 pb-24">
+
       <RenderBlocks blocks={page.layout} />
     </article>
   )
 }
+
+
